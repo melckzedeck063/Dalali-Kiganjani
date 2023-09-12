2cfee6baa3fe55dad03e7254f37eacd65c280f3b
melckzedeck <melckzedeck063@gmail.com>
2023-09-12 10:15:04 +0300

    modified app with added splash screen

---
+++ app/src/main/java/com/example/nyumbakiganjani/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.nyumbakiganjani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+    }
